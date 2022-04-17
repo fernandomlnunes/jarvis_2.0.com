88e129ec6bb02e5f08272ef69f4be225edf98294
Fernando Nunes <86544003+fernandomlnunes@users.noreply.github.com>
2022-04-16 23:32:13 -0300

    Update main_sessao.js

--- js/main_sessao.js
+++ js/main_sessao.js
@@ -7,8 +7,8 @@ let logado1 = document.querySelector('#logado1')
 let logado2 = document.querySelector('#logado2')
 let logado3 = document.querySelector('#logado3')
 let logado4 = document.querySelector('#logado4')
-logado1.innerHTML = `${userLogado1.nome}`
-logado2.innerHTML = `${userLogado2.email}`
-logado3.innerHTML = `${userLogado3.celular}`
-logado4.innerHTML = `${userLogado4.cidade}`
+logado1.innerHTML = `${userLogado1.nomeCad}`
+logado2.innerHTML = `${userLogado2.userCad}`
+logado3.innerHTML = `${N/A}`
+logado4.innerHTML = `${N/A}`
     //Eventeos
